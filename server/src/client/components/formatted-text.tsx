98ac0f182b915ffe91d168ca279c19b45ee9492f
astrellon <sovereign250@gmail.com>
2021-03-28 13:03:57 +1100

    Added format text support.

---
+++ server/src/client/components/formatted-text.tsx
@@ -0,0 +1,61 @@
+import { ClassComponent, vdom } from "simple-tsx-vdom";
+
+interface Props
+{
+    readonly text: string;
+}
+
+const formattingRegex = /(\[([^\]]+)\]\(([^\)]+)\))/;
+const globalFormattingRegex = new RegExp(formattingRegex, 'g');
+
+export default class FormattedText extends ClassComponent<Props>
+{
+    public render()
+    {
+        return <span>{this.processText()}</span>
+    }
+
+    private processText = () =>
+    {
+        const { text } = this.props;
+        const split = text.split(globalFormattingRegex);
+        const result: any[] = [];
+
+        for (let i = 0; i < split.length; i++)
+        {
+            const token = split[i];
+
+            if (!token)
+            {
+                continue;
+            }
+
+            if (token[0] === '[' && formattingRegex.test(token))
+            {
+                // Handle token
+                const type = split[i + 1];
+                const value = split[i + 2];
+                i += 2;
+
+                if (type === 'link')
+                {
+                    const linkSplit = value.split('|');
+                    const text = linkSplit[0].trim();
+                    const url = linkSplit[1].trim();
+
+                    result.push(<a href={url}>{text}</a>);
+                }
+                else if (type === 'strong')
+                {
+                    result.push(<strong>{value}</strong>);
+                }
+            }
+            else
+            {
+                result.push(<span>{token}</span>);
+            }
+        }
+
+        return result;
+    }
+}
--- server/src/client/components/post-paragraph.tsx
+++ server/src/client/components/post-paragraph.tsx
@@ -3,6 +3,7 @@ import { PostParagraphState } from "../store";
 import { PostPicture } from "./post-picture";
 import "./post-paragraph.scss";
 import PostLink from "./post-link";
+import FormattedText from "./formatted-text";
 
 interface Props
 {
@@ -19,13 +20,14 @@ export class PostParagraph extends ClassComponent<Props>
             { pictures && <div class={`post-paragraph__pictures is--${picturePosition || 'right'}`}>
                 { pictures.map(picture => <PostPicture picture={picture} />) }
             </div> }
-            { text }
+            { text && <FormattedText text={text} /> }
             { list && <ul>
-                {list.map(item => <li>{item}</li>)}
+                {list.map(item => <li><FormattedText text={item} /></li>)}
             </ul>}
             { links && <div class='post-paragraph__links'>
                 { links.map(link => <PostLink link={link} />) }
             </div> }
         </div>
     }
+
 }
