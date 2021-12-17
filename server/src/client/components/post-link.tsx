db329ee83d247b853be1ad8f08960998f299d412
astrellon <sovereign250@gmail.com>
2021-12-18 09:28:21 +1100

    Fixed trying to show a tooltip when there isn't one

--- server/src/client/components/post-link.tsx
+++ server/src/client/components/post-link.tsx
@@ -13,9 +13,14 @@ export default class PostLink extends ClassComponent<Props>
     public render()
     {
         const { url, icon, title } = this.props.link;
+        const extraProps: { [key: string]: any } = {};
+        if (title)
+        {
+            extraProps.title = title;
+        }
 
         return <div class='post-link'>
-            <a href={url} target='_blank' title={title}>
+            <a href={url} target='_blank' {...extraProps}>
                 <Icon icon={icon} />
             </a>
         </div>
